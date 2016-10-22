0071669b3dfa0ce46ed5c2967cce0229151cfddf
Pravat Bhusal <mist.bhusal@gmail.com>
2016-10-21 19:17:19 -0500

    Arm up & down motor code and some comments.

--- 2016/robotC2016.c
+++ 2016/robotC2016.c
@@ -7,36 +7,37 @@ task main()
 	0 = Stationary
 
 	Servo Position Levels:
-	127 = Full upwards (Goes left)
-	-127 = Full downwards (Goes right)
+	180 = Full upwards (Goes left)
+	-180 = Full downwards (Goes right)
 	0 = Middle
-
-	Commands:
+	
 	vexRT[] refers to the Vex Joystick's buttons/joysticks
-	Booleans:
-	0 = STOP
-	1 = GO
+	
+	Binary:
+	0 = OFF
+	1 = ON
 
 	Motors & Servos Ports:
 	port1 = Unused
+	
 	port2 = [Motor] Right Wheel
 	port3 = [Motor] Left Wheel
-
+	
+	port5 = [Servo] Right claw (opening & closing)
 	port6 = [Servo] Left claw (opening & closing)
-	port7 = [Servo] Up & Down of arm
+	
+	port7 = [Motor] Up & Down of arm
+	
 	*/
-
-	// Pre-Auton:
-	motor[port5] = 0;
-	motor[port6] = 0;
 
 	//Infinite loop to continiously update the values
 	while(1 == 1)
 	{
 	//MOTORS (we use motor[] codes)
-	motor[port2] = vexRT[Ch2]; //Channel 2 (right joystick, up & down) right wheel <-- Motor 2
-	motor[port3] = vexRT[Ch3]; //Channel 3 (left joystick, up & down) left wheel <-- Motor 3
-
+	motor[port2] = vexRT[Ch2]; //Channel 2 (right joystick, up & down) right wheel <-- Motor 1
+	motor[port3] = vexRT[Ch3]; //Channel 3 (left joystick, up & down) left wheel <-- Motor 2
+	motor[port3] = vexRT[Ch1]; //Channel 1 (right joystick, left & right) up & down arm <-- Motor 3
+		
 	//SERVOS (we can use the motor[] codes for a servo, too! However, it uses angle positions rather than power level.)
 	/* Channel 6 Buttons (RIGHT TOP SIDE) [Open and close claws] */
 	if (vexRT[Btn6U] == 1) //UP Pressed (opening claw)
@@ -49,8 +50,8 @@ task main()
 		motor[port6] = 45; //Position the servo right wise (close left claw) <-- Servo 3
 	}
 	/*
-	When opening the claw, we have one side going right (thumb) and one going left (four fingers).
-	When closing the claw, we have one side going left (thumb) and one going right (four fingers).
+	When opening the claw, we have one side going right (thumb) and one going left (inversed direction)
+	When closing the claw, we have one side going left (thumb) and one going right (inversed direction)
 	*/
 	} //End while loop
 
