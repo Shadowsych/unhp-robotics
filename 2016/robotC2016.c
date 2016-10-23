6f1dcd78f51ef68eaf3eeed25f5e7f63998d5225
Pravat Bhusal <mist.bhusal@gmail.com>
2016-10-22 20:17:11 -0500

    New arm up & down movement code

--- 2016/robotC2016.c
+++ 2016/robotC2016.c
@@ -23,10 +23,10 @@ task main()
 	port2 = [Motor] Right Wheel
 	port3 = [Motor] Left Wheel
 	
+	port4 = [Motor] Up & Down of arm
+		
 	port5 = [Servo] Right claw (opening & closing)
 	port6 = [Servo] Left claw (opening & closing)
-	
-	port7 = [Motor] Up & Down of arm
 	
 	*/
 
@@ -36,7 +36,18 @@ task main()
 	//MOTORS (we use motor[] codes)
 	motor[port2] = vexRT[Ch2]; //Channel 2 (right joystick, up & down) right wheel <-- Motor 1
 	motor[port3] = vexRT[Ch3]; //Channel 3 (left joystick, up & down) left wheel <-- Motor 2
-	motor[port3] = vexRT[Ch1]; //Channel 1 (right joystick, left & right) up & down arm <-- Motor 3
+		
+	/* Channel 5 Buttons (Moving the arm up & down) */
+	if (vexRT[Btn5U] == 1) //UP pressed (going up)
+	{
+		motor[port4] = 64; //Motor goes positive up with 64 power <-- Motor3
+	} else if (vexRT[Btn5D] == 1) //DOWN pressed (going down)
+	{
+		motor[port4] = -64; //Motor goes negative down with 64 power <-- Motor 3
+	} else 
+	{
+		motor[port4] = 0; //If nothing pressed, make the motor go to power 0 <-- Motor 3	
+	}
 		
 	//SERVOS (we can use the motor[] codes for a servo, too! However, it uses angle positions rather than power level.)
 	/* Channel 6 Buttons (RIGHT TOP SIDE) [Open and close claws] */
@@ -53,6 +64,7 @@ task main()
 	When opening the claw, we have one side going right and one going left (inversed direction)
 	When closing the claw, we have one side going left and one going right (inversed direction)
 	*/
-	} //End while loop
+		
+  } //End while loop
 
 } //End main task
