d2ec6808bce669446f3a5f2cc4b4f36942c8466f
Pravat Bhusal <mist.bhusal@gmail.com>
2016-11-06 07:30:04 -0600

    Final edits to the program. [After Game-day]
    We're finally finished with BEST Robotics 2016.
    Many programmers in the team learned new skills. I'm especially proud of Abishake and Amaan (or however you spell their names) for it was their first year and they did outstanding.
    
    Next year is my last year, so let's see how that goes!

--- 2016/robotC2016.c
+++ 2016/robotC2016.c
@@ -35,25 +35,25 @@ task main()
 	while(1 == 1)
 	{
 	//MOTORS (we use motor[] codes)
-	//The vexRT[] is negative for logical tank mode (up goes forward, down goes backward)
 	motor[port2] = (vexRT[Ch2]); //Channel 2 (right joystick, up & down) right wheel <-- Motor 1
 	motor[port3] = (vexRT[Ch3]); //Channel 3 (left joystick, up & down) left wheel <-- Motor 2
 
 	/* Channel 5 Buttons (Moving the arm up & down) */
 	if (vexRT[Btn5U] == 1 && vexRT[Btn5U] != 0) //UP hold (going up)
 	{
-		motor[port4] = 42; //Motor goes up with 84 power <-- Motor3
+		motor[port4] = 42; //Motor goes down with 42 power <-- Motor3
 	} else if (vexRT[Btn5D] == 1 && vexRT[Btn5D] != 0) //DOWN hold (going down)
 	{
-		motor[port4] = -84; //Motor goes down with 84 power <-- Motor 3
+		motor[port4] = -84; //Motor goes up with 84 power <-- Motor 3
 	} else
 	{
 		motor[port4] = 0; //If nothing pressed, make the motor go to power 20 (20 not 0 because weight of claws) <-- Motor 3
 	}
 
+	/*Channel 8 Buttons (RIGHT FRONT SIDE) [Water Wheel]*/
 	if (vexRT[Btn8U] == 1  && vexRT[Btn8U] != 0) //Channel 8 UP (Water motor up)
 	{
-		motor[port5] = 127; //Motor goes positive up with 127 power <-- Motor4
+		motor[port5] = 127; //Motor goes positive down with 127 power <-- Motor4
 	} else if(vexRT[Btn8D] == 1 && vexRT[Btn8D] != 0) //Channel 8 DOWN (Water motor down)
 	{
 		motor[port5] = -127; //Motor goes positive up with 127 power <-- Motor4
@@ -66,15 +66,11 @@ task main()
 	/* Channel 6 Buttons (RIGHT TOP SIDE) [Open and close claws] */
 	if (vexRT[Btn6U] == 1) //UP Pressed (opening claw)
 	{
-		motor[port6] = 200; //Position the servo to right wise (open right claw) <-- Servo 1
+		motor[port6] = 200; //Position the servo to open (open right claw) <-- Servo 1
 	} else if (vexRT[Btn6D] == 1) //DOWN Pressed (closing claw)
 	{
-		motor[port6] = -330; //Position the servo left wise (close right claw) <-- Servo 1
+		motor[port6] = -330; //Position the servo to close tightly (close right claw) <-- Servo 1
 	}
-	/*
-	When opening the claw, we have one side going right and one going left (inversed direction)
-	When closing the claw, we have one side going left and one going right (inversed direction)
-	*/
 
   } //End while loop
 
