490204155fc7e343517365a43bcf1a54e1588365
Pravat Bhusal <pravat.bhusal@gmail.com>
2017-09-05 21:43:51 -0500

    Refurbish the program
    I want to make it easier for my robotics software team to understand
    what I wrote from last year's code. Here's the first initiative to doing
    so :star:

--- 2016/robotC2016.c
+++ 2016/robotC2016.c
@@ -2,16 +2,14 @@ task main()
 {
 	/*
 	Motors Power Levels:
-	127 = Full power forward
+	127 = Full power clockwise
 	-127 = Full power backwards
 	0 = Stationary
 
-	Servo Position Levels:
-	180 = Full upwards (Goes left)
-	-180 = Full downwards (Goes right)
+	Servo Positionioning:
+	360 = Positive Degrees positioning on a circle
+	-360 = Negative degrees positioning on a circle
 	0 = Middle
-
-	vexRT[] refers to the Vex Joystick's buttons/joysticks
 
 	Binary:
 	0 = OFF
@@ -19,59 +17,49 @@ task main()
 
 	Motors & Servos Ports:
 	port1 = Unused
-
 	port2 = [Motor] Right Wheel
 	port3 = [Motor] Left Wheel
-
 	port4 = [Motor] Up & Down of arm
-
 	port5 = [Motor] Water motor
-
 	port6 = [Servo] Opening & Closing Claw
-
+	
+	Note: vexRT[] refers to the VEX Joystick's channels (there are up to 8 channels on the joystick).
 	*/
 
-	//Infinite loop to continiously update the values
-	while(1 == 1)
-	{
-	//MOTORS (we use motor[] codes)
-	motor[port2] = (vexRT[Ch2]); //Channel 2 (right joystick, up & down) right wheel <-- Motor 1
-	motor[port3] = (vexRT[Ch3]); //Channel 3 (left joystick, up & down) left wheel <-- Motor 2
+	// Infinite while loop to continiously run the robot
+	while(1 == 1) {
+	motor[port2] = (vexRT[Ch2]); // Channel 2 (right-side joystick, up & down) for the right wheel
+	motor[port3] = (vexRT[Ch3]); // Channel 3 (left-side joystick, up & down) for the left wheel
 
-	/* Channel 5 Buttons (Moving the arm up & down) */
-	if (vexRT[Btn5U] == 1 && vexRT[Btn5U] != 0) //UP hold (going up)
-	{
-		motor[port4] = 42; //Motor goes down with 42 power <-- Motor3
-	} else if (vexRT[Btn5D] == 1 && vexRT[Btn5D] != 0) //DOWN hold (going down)
-	{
-		motor[port4] = -84; //Motor goes up with 84 power <-- Motor 3
-	} else
-	{
-		motor[port4] = 0; //If nothing pressed, make the motor go to power 20 (20 not 0 because weight of claws) <-- Motor 3
+	// Channel 5 Buttons [Roll Arm String to move Up and Down]
+	if (vexRT[Btn5U] == 1 && vexRT[Btn5U] != 0) {// Holding Channel 5 Up Button
+		motor[port4] = 42; // Port 4 Motor rolls string down with 42 power
+	} else if (vexRT[Btn5D] == 1 && vexRT[Btn5D] != 0) {// Holding Channel 5 Down Button
+		motor[port4] = -84; // Port 4 Motor rolls string up with 84 power
+	} else {
+		motor[port4] = 0; // Else, stop the Port 4 Motor
 	}
 
-	/*Channel 8 Buttons (RIGHT FRONT SIDE) [Water Wheel]*/
-	if (vexRT[Btn8U] == 1  && vexRT[Btn8U] != 0) //Channel 8 UP (Water motor up)
+	// Channel 8 Buttons [Water Wheel]
+	if (vexRT[Btn8U] == 1  && vexRT[Btn8U] != 0) // Holding Channel 8 Up Button
 	{
-		motor[port5] = 127; //Motor goes positive down with 127 power <-- Motor4
-	} else if(vexRT[Btn8D] == 1 && vexRT[Btn8D] != 0) //Channel 8 DOWN (Water motor down)
+		motor[port5] = 127; // Port 5 Motor spins clockwise with full power
+	} else if(vexRT[Btn8D] == 1 && vexRT[Btn8D] != 0) // Holding Channel 8 Down Button
 	{
-		motor[port5] = -127; //Motor goes positive up with 127 power <-- Motor4
-	} else
-	{
-	motor[port5] = 0; //Turn the motor off
+		motor[port5] = -127; // Port 5 Motor spins counter-clockwise with full power
+	} else {
+	motor[port5] = 0; // Else, stop the Port 5 Motor
 	}
 
-	//SERVOS (we can use the motor[] codes for a servo, too! However, it uses angle positions rather than power level.)
-	/* Channel 6 Buttons (RIGHT TOP SIDE) [Open and close claws] */
-	if (vexRT[Btn6U] == 1) //UP Pressed (opening claw)
-	{
-		motor[port6] = 200; //Position the servo to open (open right claw) <-- Servo 1
-	} else if (vexRT[Btn6D] == 1) //DOWN Pressed (closing claw)
-	{
-		motor[port6] = -330; //Position the servo to close tightly (close right claw) <-- Servo 1
+	// Note: we can use the motor[] code for a servo, too!
+	
+	// Channel 6 Buttons [Open and close claws]
+	if (vexRT[Btn6U] == 1) { // Press Channel 6 Up Button
+		motor[port6] = 200; // Position Port 6 servo to open the claw
+	} else if (vexRT[Btn6D] == 1) { // Press Channel 6 Down Button
+		motor[port6] = -330; // Position Port 6 Servo to close the claw
 	}
+	
+  } // End while loop
 
-  } //End while loop
-
-} //End main task
+} // End main task function
